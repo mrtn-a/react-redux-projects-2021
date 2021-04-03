6638144cde6429501c99011ed9020b2547875c13
Martyna Markiel <martyna.markiel@gmail.com>
2021-04-03 22:43:31 +0200

    reusable components and nesting

--- 01 components/src/index.js
+++ 01 components/src/index.js
@@ -2,22 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail
-				author="Sam"
-				timeAgo="Today at 4:45PM"
-				commentContent="Nice post!"
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Alex"
-				timeAgo="Yesterday at 2:20M"
-				commentContent="Good job bro, I like it!"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<div>
+					<h4>Warning</h4>
+					Are you sure?
+				</div>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					commentContent="Nice post!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Yesterday at 2:20M"
+					commentContent="Good job bro, I like it!"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };

be2c37fa0cb3c45a57077c407d788bfb98821f80
Martyna Markiel <martyna.markiel@gmail.com>
2021-04-03 21:26:07 +0200

    providing props exercise

--- 01 components/src/index.js
+++ 01 components/src/index.js
@@ -1,25 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.cats()} />
-				</a>
-
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00 PM</span>
-					</div>
-					<div className="text">Nice blog post!</div>
-				</div>
-			</div>
+			<CommentDetail
+				author="Sam"
+				timeAgo="Today at 4:45PM"
+				commentContent="Nice post!"
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Alex"
+				timeAgo="Yesterday at 2:20M"
+				commentContent="Good job bro, I like it!"
+				avatar={faker.image.avatar()}
+			/>
 		</div>
 	);
 };

80bfcf296f3165aa6cddd591b6755545ec962b75
Martyna Markiel <martyna.markiel@gmail.com>
2021-04-17 23:20:55 +0200

    dropdown component

--- 05-widgets/src/App.js
+++ 05-widgets/src/App.js
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
-	{
-		title: "What is React?",
-		content: "React is a front end js framework",
-	},
-	{
-		title: "Why use React?",
-		content: "React is a favorite JS library among engineers",
-	},
-	{
-		title: "How do you use React?",
-		content: "You use React by creating components",
-	},
+  {
+    title: "What is React?",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
 export default () => {
-	return (
-		<div>
-			{/* <Accordion items={items} /> */}
-			<Search />
-		</div>
-	);
+  const [selected, setSelected] = useState(options[0]); // set the state
+  const [showDropdown, setShowDropdown] = useState(true); // new piece of state -> default we show the dropdown
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {
+        showDropdown ? (
+          <Dropdown
+            selected={selected}
+            options={options}
+            onSelectedChange={setSelected}
+          />
+        ) : null
+        // display or not the dropdown
+      }
+    </div>
+  );
 };
---
+++ 05-widgets/src/components/Dropdown.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  // toggle CSS classes
+  const [open, setOpen] = useState(false);
+  // assign a reference
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // check if the element that is clicked on is inside of our component,
+      // if yes, just return and not do anything
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+    // check if the currently selected item = option we are currently iterating over
+    if (option.value === selected.value) {
+      return null; // <- don't render anything
+    }
+
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {/* whenever someone clicks, we call onSelectedChange with the option
+        that we are rendering */}
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div
+          // on click set to the opposite of the current value open (true or false)
+          onClick={() => setOpen(!open)}
+          // if open=true, add 'visible active', if not, empty string
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          {/* // if open=true, add 'visible active', if not, empty string */}
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
